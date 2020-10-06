0df646dfa0106cc476cea845831c89cb3dac74fd
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-06 15:00:34 +0000

    I finished practice Q2 Oct 6

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+ import java.util.Scanner; 
 /**
- *
- * @author 
+ * This program counts up to whatever the user wants
+ * @author Zach Ramirez 
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input 
+    Scanner input = new Scanner(System.in); 
+    //prompt user for a positive integer 
+    System.out.println("Please enter a positive integer"); 
+    int integer = input.nextInt(); 
+
+    //create a for loop to count to whichever number
+    for(int count = 1; count < integer; count++){
+      System.out.print(count + ",");
+    } 
+    System.out.print(integer);   
+    
     
   }
 }
